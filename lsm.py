ff21acfc18ae48b925b1859e050823f66a4146dd
kamase <kamase.main@gmail.com>
2018-05-25 11:39:41 +0900

    output first-degree equation complete

--- lsm.py
+++ lsm.py
@@ -1,34 +1,48 @@
 #coding:utf-8
 import string
 import numpy as np
-## from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 
 
-def data_set():
-    f = open('lsmdata1_test.csv','r')
+f = open('lsmdata1_test.csv','r')
 
-    all_list = list() 
+all_list = list() 
 
-    for row in f:
-        all_list.extend(row.split())
+for row in f:
+    all_list.extend(row.split())
 
-    f.close()
+f.close()
 
-    x = list()
-    y = list()
-    count = 0
 
-    for item in all_list:
-        if count % 2 == 0 :
-            x.append( all_list[count] )
-            print(all_list[count])
-        else :
-            y.append( all_list[count] )
+x = list()
+y = list()
+count = 0
 
-        count += 1
+for item in all_list:
+    
+    if count % 2 :
+        x.append(all_list[count])
+        print(all_list[count])
+    
+    else :
+        y.append( all_list[count] )
+        
+    count += 1
 
-    print(x)
-    print(y)
+array_x =[float(f) for f in np.array(x)]
+array_y =[float(f) for f in np.array(y)]
+
+
+print(type(array_x[1]),array_y)
+
+plt.scatter(array_x,array_y,label="sample")
+
+
+lsm_result = np.poly1d(np.polyfit(array_x,array_y,1))
+
+plt.plot(np.linspace(-30,30,100),lsm_result(np.linspace(-30,30,100)),label="result")
+plt.show()
+
 
 
 
