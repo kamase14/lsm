66a5ec4fc13dcf501e92db1f6042274d1bf36646
kamase <kamase.main@gmail.com>
2018-05-25 11:47:36 +0900

    tyotto syuusei

--- lsm.py
+++ lsm.py
@@ -19,7 +19,9 @@ y = list()
 count = 0
 
 for item in all_list:
-    
+   
+    # 1.14514 1.919810 みたいな感じで書かれてるcsvファイルを空白で区切ってリストに追加してる
+    # 区切った回数が偶数のときはxに要素が入る 奇数のときはyに要素が入る
     if count % 2 :
         x.append(all_list[count])
         print(all_list[count])
@@ -29,17 +31,20 @@ for item in all_list:
         
     count += 1
 
+#21行目のforループはstr型でリストに追加しているので、ここでfloat型になおす
 array_x =[float(f) for f in np.array(x)]
 array_y =[float(f) for f in np.array(y)]
 
 
-print(type(array_x[1]),array_y)
-
+#インポートしたcsvをグラフにプロット
 plt.scatter(array_x,array_y,label="sample")
 
 
+#サンプルをもとに最小二乗法の関数を計算
 lsm_result = np.poly1d(np.polyfit(array_x,array_y,1))
 
+#グラフにプロットする
+#np.linspace()は指定範囲に等間隔な点を生成する関数 使い方は調べてください
 plt.plot(np.linspace(-30,30,100),lsm_result(np.linspace(-30,30,100)),label="result")
 plt.show()
 
